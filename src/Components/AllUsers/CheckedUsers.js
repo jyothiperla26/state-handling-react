c9e29faec1e18e7f6609c0d34af342665362e22c
jyothiperla26 <jyothi.perla@everest.engineering>
2023-06-16 18:24:07 +0530

    The selected user stylings were given here

--- src/Components/AllUsers/CheckedUsers.js
+++ src/Components/AllUsers/CheckedUsers.js
@@ -3,7 +3,7 @@ import './CheckedUsers.css';
 function CheckedUsers({name,role,email,key}) {
   
   return (
-    <div className="checked-users-container">
+    <div className="checked-user-container">
         <div className="checked-margin-right"><h4>{name}</h4></div>
         <div className="checked-margin-right"><p>{role}</p></div>
         <div className="checked-margin-right"><p>{email}</p></div>

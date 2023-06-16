6491caac72a17ca1352ad6e663e4e787c1d3a084
jyothiperla26 <jyothi.perla@everest.engineering>
2023-06-16 18:26:16 +0530

    Each user was designed and it is reusable component

--- src/Components/AllUsers/UsersList.js
+++ src/Components/AllUsers/UsersList.js
@@ -4,7 +4,7 @@ import './UsersList.css';
 function UsersList({name,role,email,isChecked,key,selectUser}) {
   
   return (
-    <div className="user-container">
+    <div className="users-container">
         <div className="margin-right"><h4>{name}</h4></div>
         <div className="margin-right"><p>{role}</p></div>
         <div className="margin-right"><p>{email}</p></div>
